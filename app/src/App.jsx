e8744ed900537bba341d1ac18c6db75d03cad027
Jawad Shaikh <shaikhjawad020@gmail.com>
2024-09-22 15:44:27 +0530

    api url issue solved

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import SearchResult from './component/SearchResult';
 
 
-export const BASE_URL = "http://localhost:9000";
+export const BASE_URL = "https://foodyzone-yk47.onrender.com";
 
 const App = () => {
 
@@ -67,6 +67,51 @@ const App = () => {
   
   //   setFilteredData(filter);
   // };
+
+  // [
+  //   {
+  //   "name": "Boilded Egg",
+  //   "price": 10,
+  //   "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  //   "image": "/images/egg.png",
+  //   "type": "breakfast"
+  //   },
+  //   {
+  //   "name": "RAMEN",
+  //   "price": 25,
+  //   "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  //   "image": "/images/ramen.png",
+  //   "type": "lunch"
+  //   },
+  //   {
+  //   "name": "GRILLED CHICKEN",
+  //   "price": 45,
+  //   "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  //   "image": "/images/chicken.png",
+  //   "type": "dinner"
+  //   },
+  //   {
+  //   "name": "CAKE",
+  //   "price": 18,
+  //   "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  //   "image": "/images/cake.png",
+  //   "type": "breakfast"
+  //   },
+  //   {
+  //   "name": "BURGER",
+  //   "price": 23,
+  //   "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  //   "image": "/images/burger.png",
+  //   "type": "lunch"
+  //   },
+  //   {
+  //   "name": "PANCAKE",
+  //   "price": 25,
+  //   "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  //   "image": "/images/pancake.png",
+  //   "type": "dinner"
+  //   }
+  // ]
   
   
 
